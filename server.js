f15c8e4208e927abf682b526eb4ad19b021809c8
nkharris <harrisnk.1998@gmail.com>
2017-08-04 00:12:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get('/', function (req, res) {
 app.get('/:articlename', function (req, res) {
     //articlename == article-one
     //articles[articlename]=={} content object for article one
-    var articlename = req.params.articleName;
+    var articlename = req.params.articlename;
   res.send(createtemplate(articles[articlename]));
 });
 
