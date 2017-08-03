9cd6209325c0b4eed9a8d12f8042eeacd45b31db
nkharris <harrisnk.1998@gmail.com>
2017-08-03 22:00:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.sendFile('Article one is requested and it will not be served here');
+});
+
+app.get('/article-two', function (req, res) {
+  res.sendFile('Article two is requested and it will not be served here');
+});
+
+app.get('/article-three', function (req, res) {
+  res.sendFile('Article three is requested and it will not be served here');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
