88a5e1b37cdc6cbda4b0763393af0a2059f3653c
nkharris <harrisnk.1998@gmail.com>
2017-08-04 22:15:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,10 @@ console.log('Loaded!');
 //change the text of the main-text div
 
 var element=document.getElementById('main-text');
-element.innerHTML = 'New value'
+element.innerHTML = 'New value';
+
+//move the image
+var element=document.getElementById('img');
+img.onclick=function(){
+    img.style.marginleft='100px';
+}
