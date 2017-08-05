c31c8d706c8fe00fe2c286cdb99635b661f89793
nkharris <harrisnk.1998@gmail.com>
2017-08-05 13:23:22 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -33,7 +33,7 @@ button.onclick=function(){
      for(var i=0;i<names.length;i++){
          list +='<li>' , names[i],'</li>';
      }
-     var ui=document.getElementById('namelist');
+     var ul=document.getElementById('namelist');
      ul.innerHTML=list;
      
  };
