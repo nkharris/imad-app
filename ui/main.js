ac151cd99670e9aabca9dfc91ffdde7c1cc3f829
nkharris <harrisnk.1998@gmail.com>
2017-08-05 13:18:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,3 +19,22 @@ button.onclick=function(){
   request.open('GET', 'http://harrisnk1998.imad.hasura-app.io/counter', true);
   request.send(null);
  };
+ 
+ //submit name
+ var nameInput=document.getElementById('name');
+ var name=nameInput.value;
+ var submit=document.getElemenyById('');
+ submit.onclick=function(){
+     //make req to seerver n send the name
+     
+     //capture a list of names and render it as a list
+     var names=['name1','name2','name3','name4'];
+     var list='';
+     for(var i=0;i<names.length;i++){
+         list +='<li>' , names[i],'</li>';
+     }
+     var ui=document.getElementById('namelist');
+     ul.innerHTML=list;
+     
+ };
+ 
