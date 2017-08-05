8f285a110d41182117c3be3f714e7dfa98c1c711
nkharris <harrisnk.1998@gmail.com>
2017-08-05 14:15:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,13 +4,13 @@ var button=document.getElementById('counter');
 button.onclick=function(){  
   //Create a request
   var request=new XMLHttpRequest();
+  
+  //capture the response and store it in a variablr
   request.onreadystatechange = function(){
       if(request.readystate===XMLHttpRequest.Done){
           //take some action
           if(request.status===200){
-            var counter=request.responseText;
-            var span=document.getElementById('count');
-            span.innerHTML=counter.toString();
+            
           }
       }
       // not done yet
@@ -25,16 +25,35 @@ button.onclick=function(){
  var name=nameInput.value;
  var submit=document.getElementById('submit_btn');
  submit.onclick=function(){
-     //make req to seerver n send the name
-     
-     //capture a list of names and render it as a list
-     var names=['name1','name2','name3','name4'];
-     var list='';
-     for(var i=0;i<names.length;i++){
-         list +='<li>' + names[i] + '</li>';
-     }
-     var ul=document.getElementById('namelist');
-     ul.innerHTML=list;
+
+     //Create a request
+  var request=new XMLHttpRequest();
+  
+  //capture the response and store it in a variablr
+  request.onreadystatechange = function(){
+      if(request.readystate===XMLHttpRequest.Done){
+          //take some action
+          if(request.status===200){
+            //capture a list of names and render it on a list
+            var names=request.responseText;
+            names=JSON.parse(names);
+            var list='';
+            for(var i=0;i<names.length;i++){
+                list+='<li>'+names[i]+'</li>';
+            }
+            var ul=document.getElementById('namelist');
+            ul.innerHTML=list;
+            
+          }
+      }
+      // not done yet
+  };
+  //make the request
+  
+  request.open('GET', 'http://harrisnk1998.imad.hasura-app.io/submit-name?name'+name, true);
+  request.send(null);
+  
+  
      
  };
  
