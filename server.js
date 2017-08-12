4a1ebc10a50c493ad01dbde52570678b432e230d
nkharris <harrisnk.1998@gmail.com>
2017-08-12 16:02:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -128,12 +128,12 @@ app.get('/submit-name',function(req,res){
     res.send(JSON.stringify( names));
 });
 
-app.get(`/articles/:articleName`, function (req, res) {
+app.get('/articles/:articleName', function (req, res) {
     //articlename == article-one
     //articles[articlename]=={} content object for article one
    
    //select * from article where title='article-one' 
-  pool.query('SELECT * FROM article WHERE title=`'+req.params.articleName + '`',function(err,result){
+  pool.query('SELECT * FROM article WHERE title= `' +req.params.articleName + '`',function(err, result){
       if(err){
           res.status(500).send(err.toString());
       }else {
