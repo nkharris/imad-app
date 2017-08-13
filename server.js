9e523e954b42c8579716a4c023b60fa1143980d5
nkharris <harrisnk.1998@gmail.com>
2017-08-13 14:22:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -140,12 +140,14 @@ app.get('/articles/:articleName', function (req, res) {
           res.status(500).send(err.toString());
       }else 
           {
-          
+          if(result.rows.length===0){
+              res.status(404).send('Article not found');
+          }else{
               var articleData=result.rows[0];
               res.send(createTemplate(articleData));
           }
       }
-  );
+  });
 });
 
 
