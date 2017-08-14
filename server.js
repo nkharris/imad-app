1423d4ab4e98ddb5631575ec277c06cbe8f58922
nkharris <harrisnk.1998@gmail.com>
2017-08-14 20:21:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -145,8 +145,8 @@ app.get('/articles/:articleName', function (req, res) {
           res.status(500).send(err.toString());
       }else 
           {
-          if(result.rows.length===0){
-              res.status(401).send('Article not found');
+          if(result.rows.length!==0){
+              res.status(404).send('Article not found');
           }else{
               var articleData=result.rows[0];
               res.send(createTemplate(articleData));
