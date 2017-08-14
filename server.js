9544fd7a7d4bb7998d889eea24759030007080f6
nkharris <harrisnk.1998@gmail.com>
2017-08-14 19:30:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,10 @@ app.get('/submit-name',function(req,res){
     //json   
     res.send(JSON.stringify( names));
 });
-
+app.get('/:articleName',function (req,res){
+    var articleName= req.params.articleName;
+    res.send(createTemplate (articles[articleName]));
+});
 app.get('/articles/:articleName', function (req, res) {
     //articlename == article-one
     //articles[articlename]=={} content object for article one
