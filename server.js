c05178d9f0021ee0972f87121ff4b26154348418
nkharris <harrisnk.1998@gmail.com>
2017-08-15 12:11:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,6 +57,29 @@ app.get('/', function (req, res) {
 });
 
 var pool=new Pool(config);
+
+app.get('/articles/:articleName', function (req, res) {
+    //articlename == article-one
+    //articles[articlename]=={} content object for article one
+   
+   //select * from article where title='\';DELETE WHERE a=\'asdf' 
+  pool.query("SELECT * FROM article WHERE title = $1 ",  [req.params.articleName] ,function(err, result)
+     {
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }else 
+          {
+          if(result.rows.length===0){
+              res.status(404).send('Article not found');
+          }else{
+              var articleData=result.rows[0];
+              res.send(createTemplate(articleData));
+          }
+      }
+  });
+});
+
 app.get('/test-db', function(req,res){
   // make a select req
   //return a response
@@ -84,28 +107,6 @@ app.get('/submit-name',function(req,res){
     names.push(name);
     //json   
     res.send(JSON.stringify( names));
-});
-
-app.get('/articles/:articleName', function (req, res) {
-    //articlename == article-one
-    //articles[articlename]=={} content object for article one
-   
-   //select * from article where title='\';DELETE WHERE a=\'asdf' 
-  pool.query("SELECT * FROM article WHERE title = $1 ",  [req.params.articleName] ,function(err, result)
-     {
-      if(err)
-      {
-          res.status(500).send(err.toString());
-      }else 
-          {
-          if(result.rows.length===0){
-              res.status(404).send('Article not found');
-          }else{
-              var articleData=result.rows[0];
-              res.send(createTemplate(articleData));
-          }
-      }
-  });
 });
 
 
