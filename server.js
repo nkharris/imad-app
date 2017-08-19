0e62a57621c21470c5e003daf9a29085556d0759
nkharris <harrisnk.1998@gmail.com>
2017-08-19 13:33:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,6 +76,7 @@ app.get('/hash/:input',function(req,res){
 
 app.post('/create-user',function(req,res){
   //username,password
+  //{"username":"harris","password":"password"}
   //JSON 
   var username=req.body.username;
   var password=req.body.password;
