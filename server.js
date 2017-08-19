9440b0d7cd3cd1e2e3dc5046f4008d7d4ba9f6f7
nkharris <harrisnk.1998@gmail.com>
2017-08-19 16:13:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,6 +141,12 @@ app.get('/check-login',function(req,res){
    
 });
 
+app.get('/logout',function(req,res){
+   delete req.session.auth;
+   res.send('logged out');
+});
+
+
 var pool=new Pool(config);
 
 app.get('/articles/:articleName', function (req, res) {
