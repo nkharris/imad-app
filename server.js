a46d2115aac77dffc7fa0b600162ff441b1329ab
nkharris <harrisnk.1998@gmail.com>
2017-08-19 13:40:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ app.post('/create-user',function(req,res){
   var username=req.body.username;
   var password=req.body.password;
   
-  var salt=crypto.randomBytes(128).tostring('hex');
+  var salt=crypto.randomBytes(128).toString('hex');
   var dbstring=hash(password,salt);
   pool.query('INSER INTO "user"(username,password) VALUES($1,$2',[username,dbString],function(err,result) {
       if(err){
