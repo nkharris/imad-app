5fcf672a9dd4046f8a9a9f1b828c8f66b203af0c
nkharris <harrisnk.1998@gmail.com>
2017-08-19 18:28:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -200,7 +200,7 @@ app.post('/submit-comment/:articleName', function (req, res) {
                             if (err) {
                                 res.status(500).send(err.toString());
                             } else {
-                                res.status(200).send('Comment inserted!');
+                                res.status(200).send('Comment inserted!')
                             }
                         });
                 }
@@ -219,8 +219,7 @@ app.get('/articles/:articleName', function (req, res) {
     } else {
         if (result.rows.length === 0) {
             res.status(404).send('Article not found');
-        } 
-        else {
+        } else {
             var articleData = result.rows[0];
             res.send(createTemplate(articleData));
         }
